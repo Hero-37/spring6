b0a3f1a4222259fc7ed7c94881b58ed9935d1000
YuLong <1003716039@qq.com>
2024-03-16 01:02:57 +0800

    2024-03-16 搭建子模块 spring6-ioc-xnm

---
+++ spring6-ioc-xml/src/main/java/com/atguigu/spring6/iocxml/User.java
@@ -0,0 +1,12 @@
+package com.atguigu.spring6.iocxml;
+
+public class User {
+
+    private String name;
+
+    private String age;
+
+    public void run() {
+        System.out.println("run......");
+    }
+}
