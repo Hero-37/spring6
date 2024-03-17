9a74e4297ce262f2ca8bee18f22cdf42e9d71f01
YuLong <1003716039@qq.com>
2024-03-18 00:29:45 +0800

    2024-03-16 注入空值属性

--- spring6-ioc-xml/src/main/java/com/atguigu/spring6/iocxml/di/Book.java
+++ spring6-ioc-xml/src/main/java/com/atguigu/spring6/iocxml/di/Book.java
@@ -6,6 +6,8 @@ public class Book {
 
     private String author;
 
+    private String others;
+
     public Book() {
         System.out.println("无参构造.......");
     }
@@ -32,11 +34,20 @@ public class Book {
         this.author = author;
     }
 
+    public String getOthers() {
+        return others;
+    }
+
+    public void setOthers(String others) {
+        this.others = others;
+    }
+
     @Override
     public String toString() {
         return "Book{" +
                 "bname='" + bname + '\'' +
                 ", author='" + author + '\'' +
+                ", others='" + others + '\'' +
                 '}';
     }
 
