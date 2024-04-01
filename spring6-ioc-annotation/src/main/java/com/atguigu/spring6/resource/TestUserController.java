555d8c29081f73c921214e68282b4b5005c81dc8
YuLong <1003716039@qq.com>
2024-04-02 00:58:25 +0800

    2024-04-01 配置类 替换 配置文件 实现全注解开发

--- spring6-ioc-annotation/src/main/java/com/atguigu/spring6/resource/TestUserController.java
+++ spring6-ioc-annotation/src/main/java/com/atguigu/spring6/resource/TestUserController.java
@@ -1,12 +1,17 @@
 package com.atguigu.spring6.resource;
 
+import com.atguigu.spring6.config.SpringConfig;
 import com.atguigu.spring6.resource.controller.UserController;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestUserController {
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("bean.xml");
+        // ApplicationContext context = new ClassPathXmlApplicationContext("bean.xml");
+
+        // 加载配置类，利用 配置类 代替 配置文件
+        ApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
         UserController userController = context.getBean(UserController.class);
         userController.add();
     }
