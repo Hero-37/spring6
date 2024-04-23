158f04e7e93634b14c6ade24ea803e2e281b6601
YuLong <1003716039@qq.com>
2024-04-24 01:37:02 +0800

    2024-04-24 动态代理 + xml 配置

---
+++ spring6-aop/src/main/java/com/atguigu/spring6/aop/xmlaop/CalculatorImpl.java
@@ -0,0 +1,35 @@
+package com.atguigu.spring6.aop.xmlaop;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CalculatorImpl implements Calculator {
+    @Override
+    public int add(int i, int j) {
+        int result = i + j;
+        System.out.println("方法内部 result = " + result);
+        // int k = 1/0;  // 异常通知
+        return result;
+    }
+
+    @Override
+    public int sub(int i, int j) {
+        int result = i - j;
+        System.out.println("方法内部 result = " + result);
+        return result;
+    }
+
+    @Override
+    public int mul(int i, int j) {
+        int result = i * j;
+        System.out.println("方法内部 result = " + result);
+        return result;
+    }
+
+    @Override
+    public int div(int i, int j) {
+        int result = i / j;
+        System.out.println("方法内部 result = " + result);
+        return result;
+    }
+}
